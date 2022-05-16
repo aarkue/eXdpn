541dfb9b01fe6fb213d480665cbfa3e9b762d99f
marairis <temai2606@gmail.com>
2022-05-16 21:11:23 +0200

    adopted enum and adjusted outputs

--- exdpn/guards/guard_manager.py
+++ exdpn/guards/guard_manager.py
@@ -2,11 +2,15 @@ from pandas import DataFrame
 from pm4py.objects.petri_net.obj import PetriNet
 from typing import Dict
 
+
 # idea: call Guard_Manager for each decision point to get the best possible guard model for either the default\
 # machine learning techniques (all implemented) or the selected machine learning techniques
 
 class Guard_Manager():
-    def __init__(self, dataframe: DataFrame, ml_list: list[str] = ["NN", "DT", "LG", "SVM"]) -> Dict[str, Guard]:
+    def __init__(self, dataframe: DataFrame, ml_list: list[str] = [ML_Technique.NN.name, 
+                                                                   ML_Technique.DT.name, 
+                                                                   ML_Technique.LG.name, 
+                                                                   ML_Technique.SVM.name]) -> Dict[str, Guard]:
         """Initializes all information needed for the calculation of the best guard for each decision point and /
         returns a dictionary with the list of all guards for each machine learning technique
         Args: 
@@ -20,23 +24,23 @@ class Guard_Manager():
         self.dataframe = dataframe
         self.ml_list = ml_list 
         # create list of all needed machine learning techniques to evaluate the guards
-        self.guards_list = {key: eval(key+"Guard")() for key in self.ml_list}
+        self.guards_list = {key: eval(key+"_Guard")() for key in self.ml_list}
 
-    # TODO output an output von Guard.evaluate anpassen
+
     def evaluate_guards(self) -> Dict[str, any]:
         """ Calculates for a given decision point all selected guards and returns the precision of the machine learning model, \
         using the specified machine learning techniques
         Returns:
             guards_results (Dict[str, any]): Returns a mapping of all selected machine learning techniques \
-                to the achieved precision and the corresponding model [TBD]
+                to the achieved precision and the trained model
             """
         self.guards_results = {} 
         # evaluate all selected ml techniques for all guards of the given decision point
-        for guard in self.guards_list:
-            self.guards_results[guard] = [self.guards_list[guard].evaluate(self.dataframe)] 
+        for guard_name in self.guards_list:
+            self.guards_results[guard_name] = self.guards_list[guard_name].evaluate(self.dataframe)
         return self.guards_results 
 
-    # TODO an output von evaluate_guards anpassen
+
     def get_best(self) -> tuple[str, any]:
         """ Returns "best" guard for a decision point
         Returns: 
