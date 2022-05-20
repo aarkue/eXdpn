3bb9bb5fba69d846d1373025be96dd340f580a2d
Cameron Pitsch <cameron.pitsch@rwth-aachen.de>
2022-05-20 13:36:23 +0200

    Progress on UI

--- ui/app.py
+++ ui/app.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from werkzeug import FileStorage
+
 app = Flask('eXdpn')
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/load-log", methods=["GET","POST"])
+def load_log():
+    if request.method == "POST":
+        # Get the form input from request
+        log: FileStorage = request.files['log']
+        return "<h1>Loading file...</h1>"
+    else:
+        return render_template("load-log.html")
