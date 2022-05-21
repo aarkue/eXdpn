dccf40dd5cad8383fa51af27883303a96a0f19cb
jonathansc <jonathansc@users.noreply.github.com>
2022-05-21 11:07:03 +0200

    Structural changes + bug fixes
    Changed imports; moved the DT guard to guards to avoid circular imports; removed hacky guard-class __init__ call;

--- exdpn/guards/guard_manager.py
+++ exdpn/guards/guard_manager.py
@@ -2,23 +2,22 @@ from pandas import DataFrame
 from pm4py.objects.petri_net.obj import PetriNet
 from typing import Dict
 
-from ml_technique import ML_Technique
-from guards import Guard
-# TODO: import all concrete Guard implementations
+from exdpn.guards import ML_Technique # imports all guard classes
+from exdpn.guards import Guard
 
 
 # idea: call Guard_Manager for each decision point to get the best possible guard model for either the default\
 # machine learning techniques (all implemented) or the selected machine learning techniques
 
 class Guard_Manager():
-    def __init__(self, dataframe: DataFrame, ml_list: list[str] = [ML_Technique.NN, 
-                                                                   ML_Technique.DT, 
-                                                                   ML_Technique.LG, 
-                                                                   ML_Technique.SVM]) -> Dict[str, Guard]:
+    def __init__(self, dataframe: DataFrame, ml_list: list[ML_Technique] = [ML_Technique.NN,
+                                                                            ML_Technique.DT,
+                                                                            ML_Technique.LG,
+                                                                            ML_Technique.SVM]) -> Dict[str, Guard]:
         """Initializes all information needed for the calculation of the best guard for each decision point and /
         returns a dictionary with the list of all guards for each machine learning technique
         Args: 
-            ml_list (list[str]): List of all machine learning techniques that should be evaluated, default is all \
+            ml_list (list[ML_technique]): List of all machine learning techniques that should be evaluated, default is all \
                 implemented techniques
             dataframe (DataFrame): Dataset used to evaluate the guard    
         Returns: 
@@ -26,10 +25,9 @@ class Guard_Manager():
                 mapped to the guards for the selected machine learning techniques       
         """
         self.dataframe = dataframe
-        self.ml_list = ml_list 
+        self.ml_list = ml_list
         # create list of all needed machine learning techniques to evaluate the guards
-        self.guards_list = {technique: eval(technique.name + "_Guard")() for technique in self.ml_list}
-
+        self.guards_list = {technique: technique.value() for technique in self.ml_list}
 
     def evaluate_guards(self) -> Dict[str, any]:
         """ Calculates for a given decision point all selected guards and returns the precision of the machine learning model, \
@@ -38,7 +36,7 @@ class Guard_Manager():
             guards_results (Dict[str, any]): Returns a mapping of all selected machine learning techniques \
                 to the achieved F1-score and the trained model
             """
-        self.guards_results = {} 
+        self.guards_results = {}
         # evaluate all selected ml techniques for all guards of the given decision point
         for guard_name in self.guards_list:
             # currently proposed pipeline:
@@ -49,8 +47,7 @@ class Guard_Manager():
             # decide on keeping model trained w/ train portion of data
             # or "retraining" the model w/ all data available
             pass
-        return self.guards_results 
-
+        return self.guards_results
 
     def get_best(self) -> tuple[str, any]:
         """ Returns "best" guard for a decision point
@@ -62,5 +59,5 @@ class Guard_Manager():
         #best_guard = [min(guards_results[place]) for place in guards_results.keys()]
         #best_guard = [max(guards_results[place]) for place in guards_results.keys()]
 
-        #return best_guard 
+        # return best_guard
         pass
