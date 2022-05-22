ca6abe9fb62c6e178c3df5ccb5a74dcdc190a094
marairis <temai2606@gmail.com>
2022-05-22 15:57:26 +0200

    small fix on timestamp convertion

--- exdpn/data_preprocessing/data_preprocessing.py
+++ exdpn/data_preprocessing/data_preprocessing.py
@@ -19,7 +19,8 @@ def data_preprocessing(dataframe: DataFrame) -> tuple[DataFrame]:
 
     # TODO define correct data types
     # convert timestamp to datatype "datetime"
-    dataframe["time:timestamp"] = pd.to_datetime(dataframe["time:timestamp"])
+    if "time:timestamp" in dataframe.columns:
+        dataframe["time:timestamp"] = pd.to_datetime(dataframe["time:timestamp"])
     
     # get target and feature names
     target_var = "target"
