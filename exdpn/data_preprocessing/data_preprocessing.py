414e9dcbe12af88e1059227bac442c0fa1fb2c18
marairis <temai2606@gmail.com>
2022-05-24 00:24:32 +0200

    changed structure of preprocessing

--- exdpn/data_preprocessing/data_preprocessing.py
+++ exdpn/data_preprocessing/data_preprocessing.py
@@ -1,4 +1,3 @@
-from xml.etree.ElementInclude import include
 from pandas import DataFrame
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
@@ -6,17 +5,42 @@ import pandas as pd
 import sys
 import numpy as np
 
-def data_preprocessing(dataframe: DataFrame) -> tuple[DataFrame]:
-    """ Basic preprocessing before data frames are used for machine learning modeling. Drops all columns \
-    with only NaN's, defines feature variables and target variable, performes MinMax scaling to [0, 1] \
-    and splits data into training and test sets.
+def data_preprocessing_evaluation(dataframe: DataFrame) -> tuple[DataFrame]:
+    """ Basic preprocessing before dataframes are used for machine learning modeling, splits data \
+    into training and test sets.
     Args:
-        dataframe (DataFrame): Dataframe to be transformed
+        dataframe (DataFrame): Dataframe to be transformed for evaluation of the best model
     Returns: 
         X_train, X_test, y_train, y_test (DataFrame): Preprocessed and splitted data
     """
 
-    # TODO define correct data types
+    # perform basic preprocessing
+    df_X, df_y = basic_data_preprocessing(dataframe)
+
+    # split data
+    X_train, X_test, y_train, y_test = train_test_split(df_X, df_y)
+
+    # define scaler on trainings data only to reduce bias 
+    # https://datascience.stackexchange.com/questions/39932/feature-scaling-both-training-and-test-data
+    data_scaler, scalable_columns = fit_scaling(X_train) 
+
+    # apply scaling on training and test data
+    X_train = apply_scaling(X_train, data_scaler, scalable_columns)
+    X_test = apply_scaling(X_test, data_scaler, scalable_columns)
+
+    return X_train, X_test, y_train, y_test
+
+def basic_data_preprocessing(dataframe: DataFrame) -> tuple[DataFrame]:
+    """ Basic preprocessing before data frames are used for machine learning modeling. Drops all columns \
+    with only NaN's, defines feature variables and target variable, performes MinMax scaling to [0, 1]
+    Args:
+        dataframe (DataFrame): Dataframe to be transformed
+    Returns: 
+        df_X (DataFrame): Preprocessed dataframe of feature variables
+        df_y (DataFrame): Preprocessed dataframe of target variable
+    """
+
+    # TODO define more correct data types? 
     # convert timestamp to datatype "datetime"
     if "event::time:timestamp" in dataframe.columns:
         dataframe["event::time:timestamp"] = pd.to_datetime(dataframe["event::time:timestamp"])
@@ -31,22 +55,11 @@ def data_preprocessing(dataframe: DataFrame) -> tuple[DataFrame]:
     # drop columns with all NaN
     df_X = df_X.dropna(how = 'all', axis = 1)
 
-    # drop case::concept:name in event logs - if existing
-    if "case::concept:name" in df_X.columns:
-        df_X = df_X.drop(["case::concept:name"], axis = 1)
+    # drop case::concept:name in event logs - if existing 
+    #if "case::concept:name" in df_X.columns:
+    #    df_X = df_X.drop(["case::concept:name"], axis = 1) 
 
-    # split data
-    X_train, X_test, y_train, y_test = train_test_split(df_X, df_y)
-
-    # define scaler on trainings data only to reduce bias 
-    # https://datascience.stackexchange.com/questions/39932/feature-scaling-both-training-and-test-data
-    data_scaler, scalable_columns = fit_scaling(X_train)
-
-    # apply scaling on training and test data
-    X_train = apply_scaling(X_train, data_scaler, scalable_columns)
-    X_test = apply_scaling(X_test, data_scaler, scalable_columns)
-
-    return X_train, X_test, y_train, y_test
+    return df_X, df_y
 
 
 def fit_scaling(X: DataFrame) -> tuple[MinMaxScaler, pd.core.indexes.base.Index]:
@@ -61,7 +74,7 @@ def fit_scaling(X: DataFrame) -> tuple[MinMaxScaler, pd.core.indexes.base.Index]
     scalable_columns = X.select_dtypes(include = [np.number]).columns
     
     # define and fit scaler
-    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaler = MinMaxScaler(feature_range = (0, 1))
     scaler.fit(X[scalable_columns])
 
     return scaler, scalable_columns
@@ -83,22 +96,26 @@ def apply_scaling(X: DataFrame, scaler: MinMaxScaler, scalable_columns: pd.core.
     return X_scaled 
 
 
-def fit_apply_ohe(X: DataFrame) -> DataFrame:
+def fit_apply_ohe(X: DataFrame, column_names: pd.core.indexes.base.Index = []) -> DataFrame:
     """ Performs One Hot Encoding on all categorical features in the data set. This is for machine learning \
     techniques that cannot handle categorical data, such as Decision Trees, SVMs and Neural Networks
     Args: 
         X (DataFrame): Dataframe with data to encode
+        column_names (pd.core.indexes.base.Index): List of column names to make One Hot Encoding persistant if new data is used
     Returns: 
-        X_encoded (DataFrame): Encoded data
+        X_encoded (DataFrame): Encoded data, if dataframe does not contain categorical data, the original \
+        dataframe is returned
     """
+    X_encoded = X.copy()
     # check if data set contains categorical data, if yes: perform one hot encoding, no: skip
     if len(X.select_dtypes(include = [object]).columns) == 0:
-        sys.exit("Data does not contain categorical data, no One Hot Encoding is necessary")
+        return X_encoded 
     else: 
         # split data into categorical and non-categorical features
-        # split data into categorical and non-categorical features
-        X_encoded = X.copy()
         categorical_columns = X_encoded.select_dtypes(include = [object]).columns
         X_encoded = pd.get_dummies(X_encoded, columns = categorical_columns)
+
+        if list(column_names):
+            X_encoded = X_encoded.reindex(columns = column_names)
         
         return X_encoded
