4536e262ee39d5471475f022384eae943af91f57
aarkue <mail@aarkue.eu>
2022-06-02 15:41:06 +0200

    Fixed sklearn version requirement
    Co-authored-by: cpitsch <cpitsch@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ setup(
     version='0.0.1',
     description='Tool to mine and evaluate explainable data Petri nets using different classification techniques.',
     install_requires=[
-        'pm4py==2.2.20.1',
-        'sklearn==1.0.2',
-        'shap==0.40.0',
-        'pandas==1.4.2',
+        'pm4py',
+        'sklearn',
+        'shap',
+        'pandas',
     ],
     test_suite='tests'
     
