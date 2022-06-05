07310214fb28b5251f85d1f653b730569c1fb344
jonathansc <jonathansc@users.noreply.github.com>
2022-06-05 23:02:41 +0200

    Lets make proper tests (this one takes time)

--- tests/data_petri_net/data_petri_net_mean_conformance.py
+++ tests/data_petri_net/data_petri_net_mean_conformance.py
@@ -9,18 +9,19 @@ import os
 def get_p2p_event_log():
     return import_xes(os.path.join(
         os.getcwd(), 'datasets', 'p2p_base.xes'))
-        #os.getcwd(), 'tests', 'guard_datasets', 'example.xes'))
 
 
 class Test_Data_Petri_Net_Mean_Conformance(unittest.TestCase):
     def test_simple(self):
+        # This test takes time..
         event_log = get_p2p_event_log()
         dpn = Data_Petri_Net(event_log, case_level_attributes=['concept:name'], event_attributes=[
-                             "item_id", "item_amount", "supplier", "item_category"], verbose=False)
-                             #"costs"], verbose=False)   
-        print(dpn.get_mean_guard_conformance(event_log))
+                             "total_price", "item_id", "item_amount", "supplier", "item_category"], verbose=True)
 
-        self.assertEqual(1,1)
+        dpn_bad = Data_Petri_Net(event_log, case_level_attributes=['concept:name'], event_attributes=[
+                             "item_amount", "supplier"], verbose=True)
+
+        self.assertLessEqual(dpn_bad.get_mean_guard_conformance(event_log), dpn.get_mean_guard_conformance(event_log))
 
 
 if __name__ == "__main__":
