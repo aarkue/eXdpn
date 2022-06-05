8d028498d2d8d1eeb7cdde422fe54921135073a3
jonathansc <jonathansc@users.noreply.github.com>
2022-06-05 22:56:03 +0200

    Initial mean guard conformance check

--- exdpn/data_petri_net/data_petri_net.py
+++ exdpn/data_petri_net/data_petri_net.py
@@ -1,6 +1,8 @@
 from pm4py.objects.petri_net.obj import PetriNet
 from pm4py.objects.log.obj import EventLog
+from pm4py.algo.conformance.tokenreplay import algorithm as token_replay
 from typing import Dict
+from tqdm import tqdm
 
 from exdpn.decisionpoints import find_decision_points
 from exdpn.guard_datasets import get_all_guard_datasets
@@ -8,6 +10,7 @@ from exdpn.guards.guard import Guard
 from exdpn.petri_net import get_petri_net
 from exdpn.guards import Guard_Manager
 from exdpn.guards import ML_Technique
+from exdpn.guard_datasets import get_all_guard_datasets
 
 
 class Data_Petri_Net():
@@ -38,7 +41,7 @@ class Data_Petri_Net():
         self.verbose = verbose
         if petri_net == None or initial_marking == None or final_marking == None:
             self.petri_net, self.im, self.fm = get_petri_net(event_log)
-        else: 
+        else:
             self.petri_net = petri_net
             self.im = initial_marking
             self.fm = final_marking
@@ -48,6 +51,11 @@ class Data_Petri_Net():
         self.guard_ds_per_place = get_all_guard_datasets(
             event_log, self.petri_net, self.im, self.fm, case_level_attributes, event_attributes, sliding_window_size, act_name_attr)
         self.print_if_verbose("Done")
+
+        self.case_level_attributes = case_level_attributes
+        self.event_attributes = event_attributes
+        self.sliding_window_size = sliding_window_size
+        self.act_name_attr = act_name_attr
         # TODO: remove default values in get all guard ds
 
         # initialize all gms
@@ -98,14 +106,81 @@ class Data_Petri_Net():
     def get_guard_at_place(self, place: PetriNet.Place) -> Guard:
         if self.guard_per_place == None:
             self.get_best()
-        
+
         return self.guard_per_place[place]
 
-    def evaluate_data_petri_net_a_posteriori(self, test_event_log: EventLog) -> float:
-        """Returns the a posteriori (first replay, then check conformance to guards) performance (definition TBD) on the given event log
+    def get_mean_guard_conformance(self, test_event_log: EventLog) -> float:
+        """Returns the mean conformance (percentage of traces where ALL guards were respected) from the given event log.
         Args:
             test_event_log (EventLog): The event log used to test the performance of the data Perti net"""
-        if self.guard_per_place != None:
+        if self.guard_per_place == None:
             self.get_best()
 
-        pass
+        for i in range(len(test_event_log)):
+            test_event_log[i].attributes['__trace_number__'] = i
+
+        self.print_if_verbose("-> Computing guard datasets for replay")
+        guard_datasets = get_all_guard_datasets(test_event_log,
+                                                self.petri_net, self.im, self.fm,
+                                                self.case_level_attributes +
+                                                ['__trace_number__'],
+                                                self.event_attributes,
+                                                self.sliding_window_size,
+                                                self.act_name_attr)
+        # TODO: add support for bringing your own ds with you
+
+        prediction_result = {dp: {i:1 for i in range(len(test_event_log))} for dp in guard_datasets.keys()}
+
+        for decision_point, dp_dataset in guard_datasets.items():
+            if len(dp_dataset) == 0:
+                    continue
+
+            cols_to_keep = [col for col in dp_dataset.columns
+                            if any(feature.startswith(col) for feature in self.guard_per_place[decision_point].feature_names)]
+            trace_nums = dp_dataset['case::__trace_number__']
+            X = dp_dataset[cols_to_keep]
+            y = list(dp_dataset["target"])
+
+            prediction = self.guard_per_place[decision_point].predict(X)
+    
+            for j in range(len(y)):
+                if y[j] != prediction[j]: 
+                    prediction_result[decision_point][trace_nums[j]] = 0
+
+        trace_performance = 0
+        for i in tqdm(range(len(test_event_log))):
+            conform = 1
+            for conform_at_dp in prediction_result.values():
+                if conform_at_dp[i] == 0:
+                    conform = 0
+                    break
+            trace_performance = trace_performance + conform
+            
+
+        #trace_performance = 0
+
+        #for i in tqdm(range(len(test_event_log))):
+        #    conformance = 1 # no guards passed leads to a conformance value of 1
+        #    for decision_point, dp_dataset in guard_datasets.items():
+        #        dp_dataset = dp_dataset[dp_dataset['case::__trace_number__'] == i]
+
+        #        if len(dp_dataset) == 0:
+        #            continue
+
+        #        cols_to_keep = [col for col in dp_dataset.columns
+        #                        if any(feature.startswith(col) for feature in self.guard_per_place[decision_point].feature_names)]
+        #        X = dp_dataset[cols_to_keep]
+        #        y = list(dp_dataset["target"])
+
+        #        respected = 0  # count the number of instances where this trace respected the guard
+        #        prediction = self.guard_per_place[decision_point].predict(X)
+
+        #        for j in range(len(y)):
+        #            if y[j] == prediction[j]:
+        #                respected = respected + 1
+
+        #        conformance = conformance * \
+        #            (respected / len(y))
+        #    trace_performance = trace_performance + conformance
+
+        return trace_performance / len(test_event_log)
