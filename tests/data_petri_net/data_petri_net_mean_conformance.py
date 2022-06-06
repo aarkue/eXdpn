6ce30fc8ccd01b8ad0385d39b20826d498880dca
jonathansc <jonathansc@users.noreply.github.com>
2022-06-06 11:16:22 +0200

    Extend test to also include LR as potential guard

--- tests/data_petri_net/data_petri_net_mean_conformance.py
+++ tests/data_petri_net/data_petri_net_mean_conformance.py
@@ -1,5 +1,6 @@
 from exdpn.data_petri_net import Data_Petri_Net
 from exdpn.load_event_log import import_xes
+from exdpn.guards import ML_Technique
 
 import unittest
 import os
@@ -15,10 +16,10 @@ class Test_Data_Petri_Net_Mean_Conformance(unittest.TestCase):
         # This test takes time..
         event_log = get_p2p_event_log()
         dpn = Data_Petri_Net(event_log, case_level_attributes=['concept:name'], event_attributes=[
-                             "total_price", "item_id", "item_amount", "supplier", "item_category"], verbose=True)
+                             "total_price", "item_id", "item_amount", "supplier", "item_category"], ml_list=[ML_Technique.DT, ML_Technique.LR], verbose=True)
 
         dpn_bad = Data_Petri_Net(event_log, case_level_attributes=['concept:name'], event_attributes=[
-                             "item_amount", "supplier"], verbose=True)
+                             "item_amount", "supplier"], ml_list=[ML_Technique.DT, ML_Technique.LR], verbose=True)
 
         self.assertLessEqual(dpn_bad.get_mean_guard_conformance(event_log), dpn.get_mean_guard_conformance(event_log))
 
