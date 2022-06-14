3071b9bc67e735b8109e88450126a9a9195190de
Cameron Pitsch <cameron.pitsch@rwth-aachen.de>
2022-06-14 19:18:41 +0200

    Remove Explainable representation print in DPN (Again 😉)

--- exdpn/data_petri_net/data_petri_net.py
+++ exdpn/data_petri_net/data_petri_net.py
@@ -123,8 +123,6 @@ class Data_Petri_Net():
             self.performance_per_place[place] = self.guard_manager_per_place[place].guards_results[ml_technique]
             self.print_if_verbose(
                 f"-> Best machine learning technique at decision point '{place.name}': {ml_technique} w/ performance {self.performance_per_place[place]}")
-            self.print_if_verbose(
-                guard[0].get_explainable_representation()) # use "training" model for representation
 
         return self.guard_per_place
 
