c26e5c0f244301037a30f90fabdf9addbe68b8d6
jonathansc <jonathansc@users.noreply.github.com>
2022-06-19 19:50:20 +0200

    Use "Marking" type for PN markings, small fixes for get_petri_net doc

--- exdpn/petri_net/get_petri_net.py
+++ exdpn/petri_net/get_petri_net.py
@@ -4,13 +4,13 @@
 """
 
 import pm4py
-from pm4py.objects.petri_net.obj import PetriNet
+from pm4py.objects.petri_net.obj import PetriNet, Marking
 from pm4py.objects.log.obj import EventLog
 from pm4py.algo.discovery.inductive import algorithm as inductive_miner
 from typing import Tuple
 
 
-def get_petri_net (log: EventLog, miner_type: str = "AM") -> Tuple[PetriNet, PetriNet.Place, PetriNet.Place]:
+def get_petri_net (log: EventLog, miner_type: str = "AM") -> Tuple[PetriNet, Marking, Marking]:
     """ Mines Petri Net based on given event log and returns found Petri Net.
 
     Args:
@@ -18,9 +18,9 @@ def get_petri_net (log: EventLog, miner_type: str = "AM") -> Tuple[PetriNet, Pet
         miner_type (str): Spezifies type of mining algorithm, either inductive miner ("IM") or alpha miner ("AM", default)
     
     Returns:
-        net (PetriNet): Petri Net based on input data, later used to find decision find decision points 
-        initial_marking (PetriNet.Place): Initial Marking
-        final_marking (PetriNet.Place): Final Marking 
+        * net (PetriNet): Petri Net based on input data, later used to find decision find decision points 
+        * initial_marking (Marking): Initial Marking
+        * final_marking (Marking): Final Marking 
 
     Raises:
         TypeError: If an miner_type is any other than "AM" or "IM".
@@ -31,7 +31,6 @@ def get_petri_net (log: EventLog, miner_type: str = "AM") -> Tuple[PetriNet, Pet
         >>> from exdpn.util import import_log
         >>> from exdpn import petri_net
         >>> event_log = import_log(os.path.join(os.getcwd(), 'datasets', 'p2p_base.xes'))
-        >>> #event_log = import_log('p2p_base.xes')
         >>> net, im, fm = petri_net.get_petri_net(event_log)
 
         ```
